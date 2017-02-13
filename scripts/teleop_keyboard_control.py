c99d39e125a638bd2e4b5b8f412444570f047aee
weisuopu <lwz0225@outlook.com>
2017-02-13 14:50:56 +0800

    teleop_keyboard_control done

--- scripts/teleop_keyboard_control.py
+++ scripts/teleop_keyboard_control.py
@@ -3,20 +3,38 @@
 import rospy
 import sys,select,termios,tty
 import math
+import threading
 from geometry_msgs.msg import PoseStamped
+from mavros_msgs.srv import CommandBool
+from mavros_msgs.srv import SetMode
+from mavros_msgs.msg import State
 id_uav = 1
+temp_pose = PoseStamped()
+mutexA = threading.Lock()
+current_state = State()
+is_arm_offb = False
+
+home_x = 0.0
+home_y = 0.0
+home_z = 0.0
+home_yaw = -1.57
+home_init_done = False
+
 msg = """
 Reading from keyboard and control the Quad!
-------------------------------
+--------------------------------------------------------------
 Moving around:
-  qQ           eE
-         W                   J
-         w                   j :UP
-    A a  *  d D
-         s                   k :DOWN
-         S                   K
-    Vv: arm and takeoff
-    Bb: land and disarm
+          qQ           eE
+                 W                             J
+                 w                             j :UP
+            A a  *  d D
+                 s                             k :DOWN
+                 S                             K
+                       Vv: arm and takeoff
+                       Bb: land and disarm
+            ctrl+c: exit
+--------------------------------------------------------------
+Moving around:
 """
 moveBindings = {
     'w':(0.2,0.0,0.0,0.0),
@@ -51,6 +69,22 @@ def rpy_to_q(r,p,yaw):
     Q = [w,x,y,z]
     return Q
 
+def limit_fram(fram_i):
+    if math.fabs(fram_i) > 1.5:
+        res = math.copysign(1.5,fram_i)
+    else:
+        res = fram_i
+    return res
+
+def limit_yaw(yaw):
+    if yaw > math.pi:
+        res = yaw - 2.0 * math.pi
+    elif yaw < -math.pi:
+        res = yaw + 2.0 * math.pi
+    else:
+        res = yaw
+    return res
+
 def getKey():
     tty.setraw(sys.stdin.fileno())
     select.select([sys.stdin],[],[],0)
@@ -58,37 +92,164 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+def command_send():
+    global temp_pose,id_uav
+    pub = rospy.Publisher("/mavros{0}/setpoint_position/local".format(id_uav),PoseStamped,queue_size=10)
+    send_rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        if mutexA.acquire():
+            pub.publish(temp_pose)
+            mutexA.release()
+        send_rate.sleep()
+    print"command_send exit"
+
+def arm_and_takeoff():
+    global current_state,is_arm_offb,temp_pose,mutexA,id_uav
+    if mutexA.acquire():
+        temp_pose.pose.position.z = -1.0
+        mutexA.release()
+    last_request = rospy.Time.now()
+    is_arm_offb = False
+    arming_client = rospy.ServiceProxy('/mavros{0}/cmd/arming'.format(id_uav),CommandBool)
+    set_mode_client = rospy.ServiceProxy('/mavros{0}/set_mode'.format(id_uav),SetMode)
+    print" ready for takeoffing "
+    while not rospy.is_shutdown():
+        if(current_state.mode != "OFFBOARD") and ((rospy.Time.now() - last_request) > rospy.Duration(3.0)):
+            if set_mode_client(0,"OFFBOARD").success :
+                print "无人机 %d 进入offboard模式"%id_uav
+            last_request = rospy.Time.now()
+        else:
+            if (current_state.mode == "OFFBOARD") and (not current_state.armed) and ((rospy.Time.now() - last_request) > rospy.Duration(3.0)):
+                if arming_client(True).success :
+                    print "无人机 %d 已经解锁！"%id_uav
+                    is_arm_offb = True
+                    break
+            elif current_state.armed:
+                print "无人机 %d 已经解锁！"%id_uav
+                is_arm_offb = True
+                break
+
+def land_and_disarm():
+    global current_state,is_arm_offb,temp_pose,mutexA,id_uav
+    if mutexA.acquire():
+        temp_pose.pose.position.z = 0.0
+        mutexA.release()
+    last_request = rospy.Time.now()
+    arming_client = rospy.ServiceProxy('/mavros{0}/cmd/arming'.format(id_uav),CommandBool)
+    print" ready for landing "
+    while (not rospy.is_shutdown()) and is_arm_offb :
+        if current_state.armed and ((rospy.Time.now() - last_request) > rospy.Duration(3.0)):
+            if arming_client(False).success :
+                print "无人机 %d 已经锁定～"%id_uav
+                break
+            elif not current_state.armed:
+                print "无人机 %d 已经锁定～"%id_uav
+                break
+    
+def state_cb(data):
+    global current_state
+    current_state = data
+
+def home_p_cb(data):
+    global home_x,home_y,home_z,home_yaw,home_init_done
+    home_x = data.pose.position.x
+    home_y = data.pose.position.z
+    home_z = -data.pose.position.y
+    x = data.pose.orientation.x
+    y = data.pose.orientation.y
+    z = data.pose.orientation.z
+    w = data.pose.orientation.w
+    home_yaw = -math.atan2(2*(w*y + z*x),(1 -2*(x*x + y*y)))
+    home_init_done = True
+
+def init_home_point():
+    global home_x,home_y,home_z,home_yaw,home_init_done,id_uav
+    sub_once = rospy.Subscriber("/mavros{0}/mocap/pose".format(id_uav),PoseStamped,home_p_cb)
+    rate_wait = rospy.Rate(10)
+    x = home_x
+    y = home_y
+    z = home_z
+    yaw = home_yaw
+    while not rospy.is_shutdown():
+        if home_init_done:
+            x = home_x
+            y = home_y
+            z = home_z
+            yaw = home_yaw
+            sub_once.unregister()
+            break
+        else:
+            rate_wait.sleep()
+    home_p = [x,y,z,yaw]
+    print "[ home_init ] done position: %.2f %.2f %.2f yaw: %.2f"%(x,y,z,yaw)
+    return home_p
+
+def task_main():
+    global msg,temp_pose,id_uav,is_arm_offb 
+    rospy.Subscriber("/mavros{0}/state".format(id_uav),State,state_cb)
+    home = init_home_point()
+    x = home[0]
+    y = home[1]
+    z = home[2]
+    yaw = home[3]
+    if mutexA.acquire():
+        temp_pose.pose.position.x = x
+        temp_pose.pose.position.y = y
+        temp_pose.pose.position.z = z
+        orient_sp = rpy_to_q(0.0,0.0,yaw)
+        temp_pose.pose.orientation.w = orient_sp[0]
+        temp_pose.pose.orientation.x = orient_sp[1]
+        temp_pose.pose.orientation.y = orient_sp[2]
+        temp_pose.pose.orientation.z = orient_sp[3]
+        mutexA.release()
+    last_time = rospy.Time.now()
+    t_step = 0.08
+    print msg
+    while(1):
+        key = getKey()
+        dt = (rospy.Time.now()-last_time).to_sec()
+        if dt > 0.05 :
+            if key in moveBindings.keys():
+                if is_arm_offb and mutexA.acquire():
+                    x = limit_fram(temp_pose.pose.position.x + moveBindings[key][0]*math.cos(yaw)*t_step- moveBindings[key][1]*math.sin(yaw)*t_step)
+                    y = limit_fram(temp_pose.pose.position.y + moveBindings[key][0]*math.sin(yaw)*t_step+ moveBindings[key][1]*math.cos(yaw)*t_step)
+                    z = limit_fram(temp_pose.pose.position.z + moveBindings[key][2]*t_step)
+                    yaw = limit_yaw(yaw + moveBindings[key][3]*t_step)
+                    temp_pose.pose.position.x = x
+                    temp_pose.pose.position.y = y
+                    temp_pose.pose.position.z = z
+                    orient_sp = rpy_to_q(0.0,0.0,yaw)
+                    temp_pose.pose.orientation.w = orient_sp[0]
+                    temp_pose.pose.orientation.x = orient_sp[1]
+                    temp_pose.pose.orientation.y = orient_sp[2]
+                    temp_pose.pose.orientation.z = orient_sp[3]
+                    mutexA.release()
+            elif (key == 'v' or key == 'V'):
+                arm_and_takeoff()
+            elif (key == 'b' or key == 'B'):
+                land_and_disarm()
+            elif (key == '\x03'):
+                break
+            last_time = rospy.Time.now()
+    print"take_main exit"
+
+def thread_init():
+    threads = []
+    t1 = threading.Thread(target = task_main)
+    t2 = threading.Thread(target = command_send)
+    threads.append(t1)
+    threads.append(t2)
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+    rospy.spin()
+
 if __name__=="__main__":
     settings = termios.tcgetattr(sys.stdin)
 
-    pub = rospy.Publisher('test_cmd',PoseStamped,queue_size=10)
     rospy.init_node('teleop_keyboard_control',anonymous=True)
-    x = 0.0
-    y = 0.0
-    z = 0.0
-    yaw = -1.57
-    temp_pose = PoseStamped()
-    k_time = 1.0/100
-    send_rate = rospy.Rate(100)
     try:
-        print msg
-        while not rospy.is_shutdown():
-            key = getKey()
-            if key in moveBindings.keys():
-                x = x + moveBindings[key][0]*math.cos(yaw)*k_time - moveBindings[key][1]*math.sin(yaw)*k_time
-                y = y + moveBindings[key][0]*math.sin(yaw)*k_time + moveBindings[key][1]*math.cos(yaw)*k_time
-                z = z + moveBindings[key][2]*k_time
-                yaw = yaw + moveBindings[key][3]*k_time
-                temp_pose.pose.position.x = x
-                temp_pose.pose.position.y = y
-                temp_pose.pose.position.z = z
-                orient_sp = rpy_to_q(0.0,0.0,yaw)
-                temp_pose.pose.orientation.w = orient_sp[0]
-                temp_pose.pose.orientation.x = orient_sp[1]
-                temp_pose.pose.orientation.y = orient_sp[2]
-                temp_pose.pose.orientation.z = orient_sp[3]
-                pub.publish(temp_pose)
-            send_rate.sleep()
+        thread_init()
     except rospy.ROSInterruptException:
         pass
     print"exit"
